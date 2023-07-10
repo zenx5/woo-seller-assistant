4ac304d757a4194f13b1742b84ef3c90734a2378
Octavio Martinez <omartinez1618@gmail.com>
2023-07-09 21:55:18 -0400

    class for zoho book connection

---
+++ classes/class-zoho-books.php
@@ -0,0 +1,22 @@
+<?php
+
+class ZohoBooks {
+
+    public static function create_invoice($data = []) {
+        $url = "https://www.zohoapis.com/books/v3/invoices?organization_id=$organization_id";
+        $headers = [
+            'Authorization:  Zoho-oauthtoken '.get_option('wsa_zoho_books_token', ''),
+            'content-type: application/json'
+        ];
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
+    }
+}
