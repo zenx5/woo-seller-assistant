16a0a13f38606b7ac7d9b092f600e70308da2547
Octavio Martinez <omartinez1618@gmail.com>
2023-07-10 18:53:32 -0400

    generate token settings

--- classes/class-zoho-api.php
+++ classes/class-zoho-api.php
@@ -2,7 +2,7 @@
 
 class ZohoApi {
 
-    public function generate_code( $code ) {
+    public static function generate_code( $code, $field = null ) {
         $url = 'https://accounts.zoho.com/oauth/v2/token';
         $client_id = get_option('wsa_zoho_client_id', '');
         $client_secret = get_option('wsa_zoho_client_secret', '');
@@ -17,32 +17,32 @@ class ZohoApi {
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         $response = curl_exec($ch);
         curl_close($ch);
 
-        return $response;
+        return $field ? json_decode($response, true)[$field] : $response;
     }
 
-    public function refresh_token( $last_token ) {
+    public static function refresh_token( $last_token, $field = null ) {
         $url = 'https://accounts.zoho.com/oauth/v2/token';
         $client_id = get_option('wsa_zoho_client_id', '');
         $client_secret = get_option('wsa_zoho_client_secret', '');
         $data = [
+            "grant_type" => "refresh_token",
             "refresh_token" => $last_token,
             "client_id" => $client_id,
-            "client_secret" => $client_secret,
-            "code" => "refresh_token"
+            "client_secret" => $client_secret
         ];
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         $response = curl_exec($ch);
         curl_close($ch);
 
-        return $response;
+        return $field ? json_decode($response, true)[$field] : $response;
     }
 }
